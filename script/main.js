5806e8936a619d53162b4bed923f6635876b05ed
claudeyang26 <90658183+Claude-Yang@users.noreply.github.com>
2021-12-13 07:24:44 -0500

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,11 @@
+function move() {
+    var element = document.getElementById("intro");
+    element.scrollIntoView({ behavior: 'smooth' });
+}
+
+function back() {
+    var element1 = document.getElementById("back");
+    element1.scrollIntoView({ behavior: 'smooth' });
+    console.log("11111")
+}
+
